1957fe3d5f34655107b7fdb8994301689375ca00
Saodat99 <134744972+Saodat99@users.noreply.github.com>
2023-06-20 17:56:26 -0400

    cange=d

---
+++ data.py
@@ -0,0 +1,16 @@
+data = [
+    {
+        "email": "saodat@gmail.com",
+        "name": "Saodat Abdusamadova",
+        "date_joined": "4/20/223", 
+        "admin": "False",
+        "id": "99"
+    },
+    {
+        "email": "kozma.cf",
+        "name": "Edgor Howell", 
+        "date_joined": "2/2/2022",
+        "admin": "False", 
+        "id": "40"
+    }
+]
